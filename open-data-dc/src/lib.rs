2fdc84bc4ef7d02d186425ce26f96eba4c415e7d
Michael Johnson <mistermichaellll@gmail.com>
2023-10-03 13:55:41 -0400

    add open-data project

---
+++ open-data-dc/src/lib.rs
@@ -0,0 +1,82 @@
+use serde::{Deserialize};
+
+#[derive(Debug, Deserialize)]
+pub struct Feature {
+    pub attributes: Crashes,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ApiResponse {
+    pub features: Vec<Feature>,
+    // Add other fields as needed
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Crashes {
+    #[serde(rename = "CRIMEID")]
+    pub crime_id:String,
+    #[serde(rename = "REPORTDATE")]
+    pub report_date:i64,
+    #[serde(rename = "ADDRESS")]
+    pub address:String,
+    #[serde(rename = "LATITUDE")]
+    pub latitude:f64,
+    #[serde(rename = "LONGITUDE")]
+    pub longitude:f64,
+    #[serde(rename = "XCOORD")]
+    pub x_coord:f64,
+    #[serde(rename = "YCOORD")]
+    pub y_coord:f64,
+    #[serde(rename = "WARD")]
+    pub ward:String,
+    #[serde(rename = "MAJORINJURIES_BICYCLIST")]
+    pub major_injuries_bicyclist:i64,
+    #[serde(rename = "MINORINJURIES_BICYCLIST")]
+    pub minor_injuries_bicyclist:i64,
+    #[serde(rename = "UNKNOWNINJURIES_BICYCLIST")]
+    pub unknown_injuries_bicyclist:i64,
+    #[serde(rename = "FATAL_BICYCLIST")]
+    pub fatal_bicyclist:i64,
+    #[serde(rename = "MAJORINJURIES_DRIVER")]
+    pub major_injuries_driver:i64,
+    #[serde(rename = "MINORINJURIES_DRIVER")]
+    pub minor_injuries_driver:i64,
+    #[serde(rename = "UNKNOWNINJURIES_DRIVER")]
+    pub unknown_injuries_driver:i64,
+    #[serde(rename = "FATAL_DRIVER")]
+    pub fatal_driver:i64,
+    #[serde(rename = "MAJORINJURIES_PEDESTRIAN")]
+    pub major_injuries_pedestrian:i64,
+    #[serde(rename = "MINORINJURIES_PEDESTRIAN")]
+    pub minor_injuries_pedestrian:i64,
+    #[serde(rename = "UNKNOWNINJURIES_PEDESTRIAN")]
+    pub unknown_injuries_pedestrian:i64,
+    #[serde(rename = "FATAL_PEDESTRIAN")]
+    pub fatal_pedestrian:i64,
+    #[serde(rename = "TOTAL_VEHICLES")]
+    pub total_vehicles:i64,
+    #[serde(rename = "TOTAL_BICYCLES")]
+    pub total_bicycles:i64,
+    #[serde(rename = "TOTAL_PEDESTRIANS")]
+    pub total_pedestrians:i64,
+    #[serde(rename = "PEDESTRIANSIMPAIRED")]
+    pub pedestrians_impaired:i64,
+    #[serde(rename = "BICYCLISTSIMPAIRED")]
+    pub bicylists_impaired:i64,
+    #[serde(rename = "DRIVERSIMPAIRED")]
+    pub drivers_impaired:i64,
+    #[serde(rename = "TOTAL_TAXIS")]
+    pub total_taxis:i64,
+    #[serde(rename = "TOTAL_GOVERNMENT")]
+    pub total_government:i64,
+    #[serde(rename = "FATALPASSENGER")]
+    pub fatal_passanger:f64,
+    #[serde(rename = "MAJORINJURIESPASSENGER")]
+    pub major_injuries_passenger:f64,
+    #[serde(rename = "MINORINJURIESPASSENGER")]
+    pub minor_injuries_passenger:f64,
+    #[serde(rename = "UNKNOWNINJURIESPASSENGER")]
+    pub unknown_injuries_passenger:f64,
+    #[serde(rename = "LASTUPDATEDATE")]
+    pub last_update_date:Option<i64>
+}
---
+++ open-data-dc/src/main.rs
@@ -0,0 +1,76 @@
+use serde::{Deserialize};
+use reqwest::Error;
+use serde_json::{Value, from_str};
+use open_data_dc::ApiResponse;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = build_crashes_url();
+
+    let response_text: String = reqwest::get(url)
+        .await?
+        .text()
+        .await?;
+
+    let api_response: ApiResponse = serde_json::from_str(&response_text).expect("Failed to parse");
+
+    for feature in api_response.features {
+        println!("CRIMEID: {}", feature.attributes.crime_id);
+        println!("REPORTDATE: {}", feature.attributes.report_date);
+        println!("ADDRESS: {}", feature.attributes.address);
+        println!("LATITUDE: {}", feature.attributes.latitude);
+        println!("LONGITUDE: {}", feature.attributes.longitude);
+        println!("TOTAL CYCLISTS: {}", feature.attributes.total_bicycles);
+        println!("--------------------------------------------");
+    }
+
+    Ok(())
+}
+
+fn build_crashes_url() -> String {
+    let mut url = String::from("https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Public_Safety_WebMercator/MapServer/24/query?where=1%3D1");
+
+    // we can adjust this as-needed, but generally these are the fields I want.
+    let field_names: [&str; 33] = [
+        "CRIMEID",
+        "REPORTDATE",
+        "ADDRESS",
+        "LATITUDE",
+        "LONGITUDE",
+        "XCOORD",
+        "YCOORD",
+        "WARD",
+        "MAJORINJURIES_BICYCLIST",
+        "MINORINJURIES_BICYCLIST",
+        "UNKNOWNINJURIES_BICYCLIST",
+        "FATAL_BICYCLIST",
+        "MAJORINJURIES_DRIVER",
+        "MINORINJURIES_DRIVER",
+        "UNKNOWNINJURIES_DRIVER",
+        "FATAL_DRIVER",
+        "MAJORINJURIES_PEDESTRIAN",
+        "MINORINJURIES_PEDESTRIAN",
+        "UNKNOWNINJURIES_PEDESTRIAN",
+        "FATAL_PEDESTRIAN",
+        "TOTAL_VEHICLES",
+        "TOTAL_BICYCLES",
+        "TOTAL_PEDESTRIANS",
+        "PEDESTRIANSIMPAIRED",
+        "BICYCLISTSIMPAIRED",
+        "DRIVERSIMPAIRED",
+        "TOTAL_TAXIS",
+        "TOTAL_GOVERNMENT",
+        "LASTUPDATEDATE",
+        "FATALPASSENGER",
+        "MAJORINJURIESPASSENGER",
+        "MINORINJURIESPASSENGER",
+        "UNKNOWNINJURIESPASSENGER"
+    ];
+
+    let out_fields = field_names.join(",");
+
+    url.push_str(&format!("&outfields={}", out_fields));
+    url.push_str("&returnGeometry=false&outSR=4326&f=json");
+
+    url
+}
